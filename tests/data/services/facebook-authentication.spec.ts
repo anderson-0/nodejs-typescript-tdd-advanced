9a760b9eb25237b68aaacbfe0870ef728f9dcb63
Anderson <anderson.consultant.it@outlook.com>
2022-02-13 17:01:37 -0500

    test: ensure FacebookAuthenticationService calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,4 +1,5 @@
 import { ILoadFacebookUserApi } from '@/data/contracts/apis';
+import { ILoadUserAccountRepository } from '@/data/contracts/repositories/user-account';
 import { FacebookAuthenticationService } from '@/data/service/facebook-authentication';
 import { AuthenticationError } from '@/domain/errors';
 
@@ -6,11 +7,22 @@ import { mock, MockProxy } from 'jest-mock-extended';
 
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<ILoadFacebookUserApi>;
+  let loadUserAccountRepo: MockProxy<ILoadUserAccountRepository>;
   let sut: FacebookAuthenticationService;
   const token = 'any_token';
   beforeEach(() => {
     loadFacebookUserApi = mock<ILoadFacebookUserApi>();
-    sut = new FacebookAuthenticationService(loadFacebookUserApi);
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      facebookId: 'any_facebook_id'
+    });
+
+    loadUserAccountRepo = mock<ILoadUserAccountRepository>();
+    sut = new FacebookAuthenticationService(
+      loadFacebookUserApi,
+      loadUserAccountRepo
+    );
   })
   it('Should call LoadFacebookUserApi with correct params', async () => {
     await sut.perform({ token })
@@ -25,5 +37,12 @@ describe('FacebookAuthenticationService', () => {
     const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError());
+  })
+
+  it('Should call LoadUserAccountRepo when LoadFacebookUserApi returns data', async () => {
+    await sut.perform({ token })
+
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email: 'any_email@mail.com' });
+    expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1);
   })
 });
