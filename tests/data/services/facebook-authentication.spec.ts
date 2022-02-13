d6bee97f8ec2ebf32ebdf1fc738fdeb30061eed2
Anderson <anderson.consultant.it@outlook.com>
2022-02-13 16:42:00 -0500

    refactor: declared token variable to be used by all tests

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -7,21 +7,22 @@ import { mock, MockProxy } from 'jest-mock-extended';
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<ILoadFacebookUserApi>;
   let sut: FacebookAuthenticationService;
+  const token = 'any_token';
   beforeEach(() => {
     loadFacebookUserApi = mock<ILoadFacebookUserApi>();
     sut = new FacebookAuthenticationService(loadFacebookUserApi);
   })
   it('Should call LoadFacebookUserApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' });
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it('Should return AuthenticationError if LoadFacebookUserApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError());
   })
