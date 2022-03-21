19eb62e6fa14ebe1484373f3e5592e68b3b9799f
Anderson <anderson.consultant.it@outlook.com>
2022-03-20 23:00:58 -0400

    refactor: FacebookApi loadUser calls only getUserInfo, which calls the other methods

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -10,7 +10,17 @@ export class FacebookApi implements ILoadFacebookUserApi {
   ) {}
 
   async loadUser (params: ILoadFacebookUserApi.Params): Promise<ILoadFacebookUserApi.Result> {
-    const appToken = await this.httpClient.get({
+    const userInfo = await this.getUserInfo(params.token);
+
+    return {
+      facebookId: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email
+    }
+  }
+
+  private async getAppToken (): Promise<any> {
+    return this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
@@ -18,28 +28,28 @@ export class FacebookApi implements ILoadFacebookUserApi {
         grant_type: 'client_credentials'
       }
     });
+  }
 
-    const debugToken = await this.httpClient.get({
+  private async getDebugToken (clientToken: string): Promise<any> {
+    const appToken = await this.getAppToken();
+    return this.httpClient.get({
       url: `${this.baseUrl}/oauth/debug_token`,
       params: {
         access_token: appToken.access_token,
-        input_token: params.token
+        input_token: clientToken
       }
     });
+  }
 
-    const userInfo = await this.httpClient.get({
+  private async getUserInfo (clientToken: string): Promise<any> {
+    const debugToken = await this.getDebugToken(clientToken);
+    return this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
         fields: ['id', 'name', 'email'].join(','),
-        access_token: params.token
+        access_token: clientToken
       }
     });
-
-    return {
-      facebookId: userInfo.id,
-      name: userInfo.name,
-      email: userInfo.email
-    }
   }
 }
