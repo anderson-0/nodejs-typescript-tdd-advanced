78b67d12c1c1adce1653d996d92330cde2e81e8f
Anderson <anderson.consultant.it@outlook.com>
2022-03-20 22:01:45 -0400

    test: move production code to their own files

---
+++ tests/infra/apis/facebook.spec.ts
@@ -0,0 +1,32 @@
+import { FacebookApi } from '@/infra/apis';
+import { IHttpGetClient } from '@/infra/http';
+import { mock, MockProxy } from 'jest-mock-extended';
+
+describe('Facebook API', () => {
+  let clientId: string;
+  let clientSecret: string;
+  let sut: FacebookApi;
+  let httpClient: MockProxy<IHttpGetClient>;
+  beforeAll(() => {
+    httpClient = mock<IHttpGetClient>()
+  })
+
+  beforeEach(() => {
+    clientId = 'any_client_id';
+    clientSecret = 'any_client_secret';
+    sut = new FacebookApi(httpClient, clientId, clientSecret);
+  });
+
+  it('should get app token', async () => {
+    await sut.loadUser({ token: 'any_client_token' });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials'
+      }
+    })
+  });
+});
