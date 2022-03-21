42b10eba900b4cc91f5932a9b7147a130b9f8272
Anderson <anderson.consultant.it@outlook.com>
2022-03-20 22:15:30 -0400

    feat: FacebookApi gets debug token after getting access_token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -8,12 +8,15 @@ describe('Facebook API', () => {
   let sut: FacebookApi;
   let httpClient: MockProxy<IHttpGetClient>;
   beforeAll(() => {
+    clientId = 'any_client_id';
+    clientSecret = 'any_client_secret';
     httpClient = mock<IHttpGetClient>()
   })
 
   beforeEach(() => {
-    clientId = 'any_client_id';
-    clientSecret = 'any_client_secret';
+    httpClient.get.mockResolvedValueOnce({
+      access_token: 'any_app_token'
+    })
     sut = new FacebookApi(httpClient, clientId, clientSecret);
   });
 
@@ -26,6 +29,18 @@ describe('Facebook API', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  });
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/oauth/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   });
