2cf4ed921a266200d31677505dcafd4416a1f63c
Anderson <anderson.consultant.it@outlook.com>
2022-03-20 23:46:38 -0400

    test: ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/apis/http/axios-client.spec.ts
+++ tests/infra/apis/http/axios-client.spec.ts
@@ -44,5 +44,13 @@ describe('Axios HTTP Client', () => {
 
       expect(result).toEqual('any_data')
     });
+
+    it('should rethrow if axios throws', async () => {
+      fakedAxios.get.mockRejectedValueOnce(new Error('http_error'));
+
+      const promise = sut.get({ url, params });
+
+      await expect(promise).rejects.toThrow(new Error('http_error'));
+    });
   });
 });
