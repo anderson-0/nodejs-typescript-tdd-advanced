05f2d13bba44ab2c41846a184c7896929dff0bdf
Anderson <anderson.consultant.it@outlook.com>
2022-03-20 22:22:16 -0400

    test: FacebookApi gets user info

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -14,9 +14,9 @@ describe('Facebook API', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({
-      access_token: 'any_app_token'
-    })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' }) // first time httpClient.get is called
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } }) // second time httpClient.get is called
     sut = new FacebookApi(httpClient, clientId, clientSecret);
   });
 
@@ -41,6 +41,18 @@ describe('Facebook API', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  });
+
+  it('should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   });
