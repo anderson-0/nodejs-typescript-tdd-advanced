53473db2b0a496606c7fc8d34f12ca67bcfb0276
Anderson <anderson.consultant.it@outlook.com>
2022-03-21 14:49:35 -0400

    feat: apply interfaces to classes

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -2,7 +2,7 @@ import { sign } from 'jsonwebtoken';
 
 import { ITokenGenerator } from '@/data/contracts/crypto';
 
-export class JwtTokenGenerator {
+export class JwtTokenGenerator implements ITokenGenerator {
   constructor (private readonly secret: string) {}
   async generateToken (params: ITokenGenerator.Params): Promise<ITokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000;
--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IHttpGetClient } from '@/infra/http';
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements IHttpGetClient {
   async get (params: IHttpGetClient.Params): Promise<any> {
     const result = await axios.get(params.url, { params: params.params });
     return result.data;
