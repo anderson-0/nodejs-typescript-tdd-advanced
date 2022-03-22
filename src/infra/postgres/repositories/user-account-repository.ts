144404928309c1b889c9dcbbdd2404a903de5b53
Anderson <anderson.consultant.it@outlook.com>
2022-03-21 21:33:18 -0400

    feat: ensure PgUserAccountRepository returns account id on save for both create and update operations

--- src/infra/postgres/repositories/user-account-repository.ts
+++ src/infra/postgres/repositories/user-account-repository.ts
@@ -24,19 +24,23 @@ export class PostgresUserAccountRepository {
     return undefined;
   }
 
-  async saveWithFacebook (params: ISaveFacebookAccountRepository.Params): Promise<void> {
+  async saveWithFacebook (params: ISaveFacebookAccountRepository.Params): Promise<ISaveFacebookAccountRepository.Result> {
+    let id: string;
     if (params.id === undefined) {
-      await this.pgUserRepo.save({
+      const pgUser = await this.pgUserRepo.save({
         email: params.email,
         name: params.name,
         facebookId: params.facebookId
       })
+      id = pgUser.id.toString();
     } else {
       // updates should never change the email even if it is different
+      id = params.id;
       await this.pgUserRepo.update(params.id, {
         name: params.name,
         facebookId: params.facebookId
       });
     }
+    return { id };
   }
 }
