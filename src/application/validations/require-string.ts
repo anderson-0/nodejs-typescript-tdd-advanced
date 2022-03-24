a38c35f7cc19ad36ae7aa0145a9503c180a4b15b
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-24 00:23:18 -0400

    feat: moved production code to their own files

---
+++ src/application/validations/require-string.ts
@@ -0,0 +1,14 @@
+import { RequiredFieldError } from '@/application/errors';
+
+export class RequiredStringValidator {
+  constructor (
+    private readonly value: string,
+    private readonly fieldName: string
+  ) {}
+
+  validate (): Error | undefined {
+    if (['', null, undefined].includes(this.value)) {
+      return new RequiredFieldError('any_field');
+    }
+  }
+}
--- tests/application/validations/required-string.spec.ts
+++ tests/application/validations/required-string.spec.ts
@@ -1,15 +1,5 @@
+import { RequiredStringValidator } from '@/application/validations';
 import { RequiredFieldError } from '@/application/errors';
-
-class RequiredStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate (): Error | undefined {
-    return new RequiredFieldError('any_field');
-  }
-}
 
 describe('Required String Validator', () => {
   it('should return RequiredFieldError if value is empty', () => {
@@ -34,5 +24,13 @@ describe('Required String Validator', () => {
     const error = sut.validate();
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
+  });
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_value', 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined();
   });
 })
