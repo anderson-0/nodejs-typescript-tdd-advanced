a839e7e7b4208fc97454405d5b76f07e3bc7b2e9
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-23 23:45:12 -0400

    feat: implemented generics to http request and response types

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,17 +1,26 @@
 import { RequiredFieldError } from '@/application/errors';
+import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers';
+
 import { IFacebookAuthentication } from '@/domain/features';
 import { AccessToken } from '@/domain/models';
 
-import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers';
+type HttpRequest = {
+  token: string | undefined | null
+}
+
+type Model = Error | {
+  accessToken: string
+}
+
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: IFacebookAuthentication) {}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (['', null, undefined].includes(httpRequest.token)) {
         return badRequest(new RequiredFieldError('token'));
       }
-      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token });
+      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token as string });
 
       if (accessToken instanceof AccessToken) {
         return ok({
