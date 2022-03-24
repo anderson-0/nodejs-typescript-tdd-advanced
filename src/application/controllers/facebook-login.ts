2379571ee4093ee5337d58dec21811752c997aa8
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-23 23:35:22 -0400

    feat: applied ok and serverError helpers

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,9 +1,8 @@
-import { RequiredFieldError, ServerError } from '@/application/errors';
+import { RequiredFieldError } from '@/application/errors';
 import { IFacebookAuthentication } from '@/domain/features';
 import { AccessToken } from '@/domain/models';
 
-import { badRequest, HttpResponse, unauthorized } from '@/application/helpers';
-
+import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers';
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: IFacebookAuthentication) {}
 
@@ -15,20 +14,14 @@ export class FacebookLoginController {
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token });
 
       if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: accessToken.value
-          }
-        }
+        return ok({
+          accessToken: accessToken.value
+        });
       } else {
         return unauthorized()
       }
     } catch (error: any) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error)
-      }
+      return serverError()
     }
   }
 }
