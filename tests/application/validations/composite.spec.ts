7d6627410a5458b911fbad309853b1d2ab43dc81
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-25 18:21:39 -0400

    test: ensure ValidationComposite returns the first error if there is more than 1 validator

--- tests/application/validations/composite.spec.ts
+++ tests/application/validations/composite.spec.ts
@@ -36,11 +36,18 @@ describe('Validation Composite', () => {
     expect(error).toBeUndefined()
   });
 
-  it('should return the first error', async () => {
+  it('should return the first error if there is more than one', async () => {
     validator1.validate.mockReturnValueOnce(new Error('error1'));
     validator2.validate.mockReturnValueOnce(new Error('error2'));
     const error = sut.validate();
 
     expect(error).toEqual(new Error('error1'));
+  });
+
+  it('should return the first error if there is more than 1 validator', async () => {
+    validator2.validate.mockReturnValueOnce(new Error('error2'));
+    const error = sut.validate();
+
+    expect(error).toEqual(new Error('error2'));
   });
 });
