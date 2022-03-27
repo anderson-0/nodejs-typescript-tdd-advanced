8d25dc3b8a255138a64e218074b95051f15e2751
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 16:47:47 -0400

    feat: created Controller abstract class

---
+++ src/application/controllers/controller.ts
@@ -0,0 +1,26 @@
+import { badRequest, HttpResponse, serverError } from '@/application/helpers';
+import { IValidator, ValidationComposite } from '@/application/validations';
+
+export abstract class Controller {
+  abstract perform (httpRequest: any): Promise<HttpResponse>
+  buildValidators (httpRequest: any): IValidator[] {
+    return [];
+  }
+
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    try {
+      const error = this.validate(httpRequest);
+      if (error !== undefined) {
+        return badRequest(error);
+      }
+      return await this.perform(httpRequest);
+    } catch (error: any) {
+      return serverError(error);
+    }
+  }
+
+  private validate (httpRequest: any): Error | undefined {
+    const validators = this.buildValidators(httpRequest);
+    return new ValidationComposite(validators).validate();
+  }
+}
