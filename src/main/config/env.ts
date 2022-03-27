bfd33f0dc15da1e65d4312d9fc3353ddcdd182b8
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 19:03:45 -0400

    feat: included facebook credentials as environment variables

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FACEBOOK_CLIENT_ID ?? '282640477364862',
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '75da1427ec7845cee130e2b48c18f022'
+  }
+}
