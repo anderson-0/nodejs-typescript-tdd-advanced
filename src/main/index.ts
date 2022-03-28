417a4c8aa2253c3b35887091c49dea5a727bc2f1
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 22:28:31 -0400

    feat: included cors and json middlewares

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,11 @@
 import './config/module-alias';
 import 'reflect-metadata';
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
+app.use(json());
 app.listen(8080, () => {
   console.log('Server running on port 8080');
 });
