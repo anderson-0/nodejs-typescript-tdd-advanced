c6cfa8834a869fad36afafc7e30037d1d216b93e
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-28 19:16:46 -0400

    feat: moved express-router-adapter code to its own file

--- tests/infra/http/express-router-adapter.spec.ts
+++ tests/infra/http/express-router-adapter.spec.ts
@@ -1,19 +1,8 @@
-import { Request, Response } from 'express';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { mock, MockProxy } from 'jest-mock-extended';
+import { Request, Response } from 'express';
 import { Controller } from '@/application/controllers';
-export class ExpressRouterAdapter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const response = await this.controller.handle({ ...req.body });
-    if (response.statusCode === 200) {
-      res.status(200).json(response.data);
-    } else {
-      res.status(response.statusCode).json({ error: response.data.message });
-    }
-  }
-}
+import { ExpressRouterAdapter } from '@/infra/http/express-router-adapter';
 
 describe('Express Router Adapter', () => {
   let req: Request;
