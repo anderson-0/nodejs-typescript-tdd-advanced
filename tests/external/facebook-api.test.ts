f9ba865a44e665ddc2843225ecff136409da7caf
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 22:05:46 -0400

    test: moved SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,10 +3,14 @@ import { AxiosHttpClient } from '@/infra/http';
 import { env } from '@/main/config/env';
 
 describe('Facebook API Integration Tests', () => {
-  it('should return Facebook user if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
+  let axiosClient: AxiosHttpClient;
+  let sut: FacebookApi;
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
+  });
 
+  it('should return Facebook user if token is valid', async () => {
     // The test user's token is valid for 3 months
     const facebookUser = await sut.loadUser({ token: 'EAAEBD11Aon4BAInP6tc9PaISuZBpmV14X9FykeytmEQQfgE0PPsRXtk6nollcPLZAWJXfK7Ufu2F1UMbhuk1FMZAjPZCmr6FPoArK6ZADznBeEYsDpRecE71jQSex1jugv0luxYYAqkqcMzJYFYTzwpUyIV1CZBO1ZAHwcjKww7iRrPvskarpX8Sk0du4h8xzNdQyljKVOV1QZDZD' });
 
