915ad6e1b783cbc8a86850917966163025ede036
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-28 18:28:56 -0400

    test: isolate SUT creation

--- tests/infra/http/express-router-adapter.spec.ts
+++ tests/infra/http/express-router-adapter.spec.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getMockReq, getMockRes } from '@jest-mock/express';
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { Controller } from '@/application/controllers';
 export class ExpressRouterAdapter {
   constructor (private readonly controller: Controller) {}
@@ -11,15 +11,28 @@ export class ExpressRouterAdapter {
 }
 
 describe('Express Router Adapter', () => {
+  let req: Request;
+  let res: Response;
+  let controller: MockProxy<Controller>;
+  let sut: ExpressRouterAdapter
+  beforeEach(() => {
+    res = getMockRes({ body: { any: 'any' } }).res;
+    req = getMockReq({ body: { any: 'any' } });
+    controller = mock<Controller>();
+    sut = new ExpressRouterAdapter(controller);
+  });
+
   it('should call controller handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } });
-    const { res } = getMockRes();
-    const controller = mock<Controller>();
-
-    const sut = new ExpressRouterAdapter(controller);
-
     await sut.adapt(req, res);
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
+  });
+
+  it('should call controller handle with empty request', async () => {
+    req = getMockReq({ body: undefined });
+
+    await sut.adapt(req, res);
+
+    expect(controller.handle).toHaveBeenCalledWith({});
   });
 })
