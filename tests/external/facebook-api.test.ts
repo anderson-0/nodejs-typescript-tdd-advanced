05c28448107ea9d4b449351c56311f20270687ce
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 21:49:41 -0400

    test: ensure Facebook API Integration returns Facebook user if token is valid

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,19 @@
-describe('', () => {
-  it('', () => {
-    expect(true).toBe(true);
+import { FacebookApi } from '@/infra/apis';
+import { AxiosHttpClient } from '@/infra/http';
+import { env } from '@/main/config/env';
+
+describe('Facebook API Integration Tests', () => {
+  it('should return Facebook user if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient();
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
+
+    // The test user's token is valid for 3 months
+    const facebookUser = await sut.loadUser({ token: 'EAAEBD11Aon4BAInP6tc9PaISuZBpmV14X9FykeytmEQQfgE0PPsRXtk6nollcPLZAWJXfK7Ufu2F1UMbhuk1FMZAjPZCmr6FPoArK6ZADznBeEYsDpRecE71jQSex1jugv0luxYYAqkqcMzJYFYTzwpUyIV1CZBO1ZAHwcjKww7iRrPvskarpX8Sk0du4h8xzNdQyljKVOV1QZDZD' });
+
+    expect(facebookUser).toEqual({
+      facebookId: '101068395903445',
+      email: 'test_oofjfiq_user@tfbnw.net',
+      name: 'Test User'
+    });
   });
 });
