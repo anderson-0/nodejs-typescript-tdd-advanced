8efce46f7ce8fe603f45810b431394ee5d91a6a9
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 22:35:02 -0400

    feat: setup app entry file and one route

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,24 @@
 import './config/module-alias';
 import 'reflect-metadata';
-import express, { json } from 'express';
+import express, { json, Router, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.type('application/json');
+  next();
+});
+
+const router = Router();
+router.post('/api/login/facebook', (req: Request, res: Response, next: NextFunction) => {
+  res.send({
+    data: 'any_data'
+  })
+});
+app.use(router);
 app.listen(8080, () => {
   console.log('Server running on port 8080');
 });
