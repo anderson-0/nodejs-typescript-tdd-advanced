1e9e30ac7bffe63c67ccddb30b70fddf71619028
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-27 22:26:26 -0400

    feat: configured express server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,9 @@
+import './config/module-alias';
 import 'reflect-metadata';
+import express from 'express';
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
