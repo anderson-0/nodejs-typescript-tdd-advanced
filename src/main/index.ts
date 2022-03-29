4da3932f96e3de8d24ec4adc4b5cfe9b4a6cb1e9
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-29 00:13:22 -0400

    feat: configured typeorm connection

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,11 @@ import './config/module-alias';
 import 'reflect-metadata';
 import { app } from '@/main/config/app';
 import { env } from '@/main/config/env';
-app.listen(env.port, () => {
-  console.log(`Server running on port ${env.port}`);
-});
+import { createConnection } from 'typeorm';
+import { config } from '@/infra/postgres/helpers/config';
+
+createConnection(config).then(() => {
+  app.listen(env.port, () => {
+    console.log(`Server running on port ${env.port}`);
+  });
+}).catch((err) => console.log(err));
