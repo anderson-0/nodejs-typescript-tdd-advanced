992324f3b4beae4a4fc6dfeb2f7f1158cf8aa586
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-03-29 22:35:18 -0400

    refactor: added type alias to adaptExpressRoute

--- src/infra/http/express-router-adapter.ts
+++ src/infra/http/express-router-adapter.ts
@@ -1,11 +1,10 @@
 import { Request, RequestHandler, Response } from 'express';
 import { Controller } from '@/application/controllers';
 
-export const adaptExpressRoute = (controller: Controller): RequestHandler => {
-  return async (req: Request, res: Response) => {
-    const { statusCode, data } = await controller.handle({ ...req.body });
-    const jsonData = statusCode === 200 ? data : { error: data.message }
+type Adapter = (controller: Controller) => RequestHandler;
+export const adaptExpressRoute: Adapter = controller => async (req: Request, res: Response) => {
+  const { statusCode, data } = await controller.handle({ ...req.body });
+  const jsonData = statusCode === 200 ? data : { error: data.message }
 
-    res.status(statusCode).json(jsonData);
-  };
-}
+  res.status(statusCode).json(jsonData);
+};
