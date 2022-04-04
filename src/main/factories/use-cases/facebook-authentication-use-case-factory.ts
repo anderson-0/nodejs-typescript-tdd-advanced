db9cef418ceccfb2b75fa51f3f0f03c34e3fb799
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-04-03 21:06:58 -0400

    fix: fixed old invalid import to JwtTokenGenerator and switch to JwtTokenHandler

--- src/main/factories/use-cases/facebook-authentication-use-case-factory.ts
+++ src/main/factories/use-cases/facebook-authentication-use-case-factory.ts
@@ -2,14 +2,14 @@ import { env } from '@/main/config/env';
 
 import { FacebookAuthentication, setupFacebookAuthentication } from '@/domain/use-cases';
 
-import { JwtTokenGenerator } from '@/infra/crypto';
+import { JwtTokenHandler } from '@/infra/crypto';
 import { PostgresUserAccountRepository } from '@/infra/postgres/repositories';
 import { makeFacebookApi } from '../apis/facebook-api-factory';
 
 export const makeFacebookAuthenticationUseCase = (): FacebookAuthentication => {
   const postgresUserAccountRepository = new PostgresUserAccountRepository();
-  const jwtTokenGenerator = new JwtTokenGenerator(env.jwtSecret);
+  const jwtTokenHandler = new JwtTokenHandler(env.jwtSecret);
   const facebookApi = makeFacebookApi();
 
-  return setupFacebookAuthentication(facebookApi, postgresUserAccountRepository, jwtTokenGenerator);
+  return setupFacebookAuthentication(facebookApi, postgresUserAccountRepository, jwtTokenHandler);
 }
