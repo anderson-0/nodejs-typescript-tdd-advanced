c162914b1fd1f3f60569afdaf63ab1c0382cf884
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-04-03 20:43:22 -0400

    feat: created validateToken method inside JwtTokenHandler

--- src/infra/crypto/jwt-token-handler.ts
+++ src/infra/crypto/jwt-token-handler.ts
@@ -1,6 +1,6 @@
-import { sign } from 'jsonwebtoken';
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
 
-import { ITokenGenerator } from '@/domain/contracts/crypto';
+import { ITokenGenerator, ITokenValidator } from '@/domain/contracts/crypto';
 
 export class JwtTokenHandler implements ITokenGenerator {
   constructor (private readonly secret: string) {}
@@ -8,5 +8,10 @@ export class JwtTokenHandler implements ITokenGenerator {
     const expirationInSeconds = params.expirationInMs / 1000;
     const token = sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds });
     return token;
+  }
+
+  async validateToken ({ token }: ITokenValidator.Params): Promise<void> {
+    const payload = verify(token, this.secret) as JwtPayload;
+    return payload.key;
   }
 }
