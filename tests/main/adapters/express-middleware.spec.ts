31a3b547913075c7c0be49fcf6416f5c5cb96e60
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-04-24 21:32:23 -0400

    test: ensure ExpressMiddleware add valid data to req.locals

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -7,8 +7,14 @@ import { mock, MockProxy } from 'jest-mock-extended';
 type Adapter = (middleware: IMiddleware) => RequestHandler;
 
 const adaptExpressMiddleware: Adapter = (middleware) => {
-  return async (req, res, _next) => {
+  return async (req, res, next) => {
     const { statusCode, data } = await middleware.handle({ ...req.headers });
+    if (statusCode === 200) {
+      const dataEntries = Object.entries(data).filter(entry => entry[1]);
+
+      req.locals = { ...req.locals, ...Object.fromEntries(dataEntries) };
+      next();
+    }
     return res.status(statusCode).json(data);
   };
 }
@@ -31,10 +37,14 @@ describe('Express Middleware', () => {
     next = getMockRes().next;
 
     middleware = mock<IMiddleware>();
-
     middleware.handle.mockResolvedValue({
-      statusCode: 500,
-      data: { error: 'any_error' }
+      statusCode: 200,
+      data: {
+        emptyProperty: '',
+        nullProperty: null,
+        undefinedProperty: undefined,
+        prop: 'any_value'
+      }
     });
   });
 
@@ -61,8 +71,11 @@ describe('Express Middleware', () => {
     expect(middleware.handle).toHaveBeenCalledTimes(1);
   });
 
-  it('should respond with corret error and  ', async () => {
-    const sut = adaptExpressMiddleware(middleware);
+  it('should respond with corret error and status code', async () => {
+    middleware.handle.mockResolvedValueOnce({
+      statusCode: 500,
+      data: { error: 'any_error' }
+    });
 
     await sut(req, res, next);
 
@@ -70,5 +83,19 @@ describe('Express Middleware', () => {
     expect(res.status).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenCalledWith({ error: 'any_error' });
     expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  it('should add data to req.locals', async () => {
+    await sut(req, res, next);
+
+    expect(req.locals).toEqual({ prop: 'any_value' });
+    expect(next).toHaveBeenCalledTimes(1);
+  });
+
+  it('should valid data to req.locals', async () => {
+    await sut(req, res, next);
+
+    expect(req.locals).toEqual({ prop: 'any_value' });
+    expect(next).toHaveBeenCalledTimes(1);
   });
 })
