ed94f0940a2e9d75a44272fddae7c1ae9e8a1770
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-04-26 12:08:14 -0400

    test: should return 200 if a valid authorization header was not provided

--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,11 +1,23 @@
+import { sign } from 'jsonwebtoken';
 import request from 'supertest';
+
 import { app } from '@/main/config/app';
-import { ForbiddenError } from '@/application/errors';
+import { env } from '@/main/config/env';
 import { auth } from '@/main/middlewares';
 
+import { ForbiddenError } from '@/application/errors';
+
 describe('Authentication Middleware (Main Layer)', () => {
+  let userId: string;
+  let fakeRoute: string;
+
+  beforeAll(async () => {
+    userId = 'any_user_id';
+    fakeRoute = '/fake_route';
+  });
+
   it('should return 403 if authorization header is not provided ', async () => {
-    app.get('/fake_route', auth, (req, res) => {
+    app.get(fakeRoute, auth, (req, res) => {
       res.json(req.locals);
     })
 
@@ -13,5 +25,22 @@ describe('Authentication Middleware (Main Layer)', () => {
 
     expect(status).toBe(403);
     expect(body.error).toBe(new ForbiddenError().message);
+  });
+
+  it('should return 200 if a valid authorization header was not provided', async () => {
+    const authorization = sign({ key: userId }, env.jwtSecret);
+
+    app.get(fakeRoute, auth, (req, res) => {
+      res.json(req.locals);
+    })
+
+    const { status, body } = await request(app)
+      .get(fakeRoute)
+      .set({ authorization });
+
+    expect(status).toBe(200);
+    expect(body).toEqual({
+      userId
+    });
   });
 });
